bd0596df404042743428adca6a8e0a973f8e336c
Bartosz Kazmierczak <kazmierczakbartosz97@gmail.com>
2022-02-17 19:20:37 +0100

    Add initial dependencies

--- main.cpp
+++ main.cpp
@@ -1,15 +1,9 @@
-#include <iostream>
-#include <boost/log/trivial.hpp>
+#include "spdlog/spdlog.h"
 
 
-int main() {
-    BOOST_LOG_TRIVIAL(info) << "Home Automation Center Controller v1.0";
+int main()
+{
+    spdlog::info("Hello world!");
 
-    BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-    BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
-    BOOST_LOG_TRIVIAL(info) << "An informational severity message";
-    BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
-    BOOST_LOG_TRIVIAL(error) << "An error severity message";
-    BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
     return 0;
 }
